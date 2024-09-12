8d6cbff9a664b6dcac84274315542d63aef4223b
Danilov210 <Alexander.Danilov@e.braude.co.il>
2024-09-12 20:07:15 +0300

    aaaa222

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://miracle-of-luck2.vercel.app/api",
+  baseURL: "https://miracleofluck.onrender.com/api",
 });
 
 export const createUser = async (data, token) => {
